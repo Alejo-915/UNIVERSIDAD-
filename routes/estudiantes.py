c050169ca7e5c17c2e41ac8fefde3dfebde79d3f
Alejo-915 <edwinlm915@gmail.com>
2025-10-26 17:39:08 -0500

    feat:endpoint de estudiante

---
+++ routes/estudiantes.py
@@ -0,0 +1,45 @@
+from fastapi import APIRouter, HTTPException
+from sqlmodel import Session, select
+from models import Estudiante
+from database import engine
+
+router = APIRouter(prefix="/estudiantes", tags=["Estudiantes"])
+
+@router.get("/")
+def listar_estudiantes():
+    with Session(engine) as session:
+        estudiantes = session.exec(select(Estudiante)).all()
+        return estudiantes
+
+
+@router.get("/otronombre")
+def listar_estudiantes():
+    with Session(engine) as session:
+        estudiantes = session.exec(select(Estudiante)).all()
+        return estudiantes
+
+@router.post("/")
+def crear_estudiante(estudiante: Estudiante):
+    with Session(engine) as session:
+        session.add(estudiante)
+        session.commit()
+        session.refresh(estudiante)
+        return estudiante
+
+@router.get("/{estudiante_id}")
+def obtener_estudiante(estudiante_id: int):
+    with Session(engine) as session:
+        estudiante = session.get(Estudiante, estudiante_id)
+        if not estudiante:
+            raise HTTPException(status_code=404, detail="Estudiante no encontrado")
+        return estudiante
+
+@router.delete("/{estudiante_id}")
+def eliminar_estudiante(estudiante_id: int):
+    with Session(engine) as session:
+        estudiante = session.get(Estudiante, estudiante_id)
+        if not estudiante:
+            raise HTTPException(status_code=404, detail="Estudiante no encontrado")
+        session.delete(estudiante)
+        session.commit()
+        return {"mensaje": "Estudiante eliminado correctamente"}
