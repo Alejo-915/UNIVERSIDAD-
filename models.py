a078c99b03db2980fcabcb7318039cf350c49bcf
Alejo-915 <edwinlm915@gmail.com>
2025-10-26 17:52:02 -0500

    feat: creacion de clases de la base de datos

---
+++ models.py
@@ -0,0 +1,27 @@
+from sqlmodel import SQLModel, Field, Relationship
+from typing import List, Optional
+
+
+class Estudiante(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    nombre: str
+    correo: str
+    edad: int
+
+    matriculas: List["Matricula"] = Relationship(back_populates="estudiante")
+
+class Curso(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    nombre: str
+    descripcion: str
+
+    matriculas: List["Matricula"] = Relationship(back_populates="curso")
+
+class Matricula(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    estudiante_id: int = Field(foreign_key="estudiante.id")
+    curso_id: int = Field(foreign_key="curso.id")
+    fecha: str
+
+    estudiante: Optional[Estudiante] = Relationship(back_populates="matriculas")
+    curso: Optional[Curso] = Relationship(back_populates="matriculas")
