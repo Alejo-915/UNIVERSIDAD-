6637e9bbd155720584b4bb28a649906e215f7d9e
Alejo-915 <edwinlm915@gmail.com>
2025-10-26 17:54:37 -0500

    feat: registro de los routers

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
-app = FastAPI()
-app.get("/")
-app.get("/item")
-def root():
-    return {"message": "Hello World"}
+from sqlmodel import SQLModel, create_engine
+from routes import estudiantes, cursos, matricula
+
+from database import engine
+
+
+app = FastAPI(title="Universidad API")
+
+@app.on_event("startup")
+def on_startup():
+    SQLModel.metadata.create_all(engine)
+
+app.include_router(estudiantes.router)
+app.include_router(cursos.router)
+app.include_router(matricula.router)
