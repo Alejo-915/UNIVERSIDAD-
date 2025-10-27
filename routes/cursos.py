a4c2cef69cbf56b6d896452837b8e89ac94960f4
Alejo-915 <edwinlm915@gmail.com>
2025-10-26 20:53:49 -0500

    feat: filtardo de curso por credito

--- routes/cursos.py
+++ routes/cursos.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Query
 from sqlmodel import Session, select
 from models import Curso
 from database import engine
@@ -7,9 +7,12 @@ from database import engine
 router = APIRouter(prefix="/cursos", tags=["Cursos"])
 
 @router.get("/")
-def listar_cursos():
+def listar_cursos(creditos: int | None = Query(default=None, description="Filtrar cursos por número de créditos")):
     with Session(engine) as session:
-        cursos = session.exec(select(Curso)).all()
+        query = select(Curso)
+        if creditos is not None:
+            query = query.where(Curso.creditos == creditos)
+        cursos = session.exec(query).all()
         return cursos
 
 @router.post("/")
@@ -37,3 +40,22 @@ def eliminar_curso(curso_id: int):
         session.delete(curso)
         session.commit()
         return {"mensaje": "Curso eliminado correctamente"}
+
+# actualizar cuerso
+@router.put("/{curso_id}")
+def actualizar_curso(curso_id: int, datos_actualizados: Curso):
+    with Session(engine) as session:
+        curso = session.get(Curso, curso_id)
+        if not curso:
+            raise HTTPException(status_code=404, detail="Curso no encontrado")
+
+        # actualizar solo los campos enviados
+        if datos_actualizados.nombre:
+            curso.nombre = datos_actualizados.nombre
+        if datos_actualizados.descripcion:
+            curso.descripcion = datos_actualizados.descripcion
+
+        session.add(curso)
+        session.commit()
+        session.refresh(curso)
+        return {"mensaje": "Curso actualizado correctamente", "curso": curso}
